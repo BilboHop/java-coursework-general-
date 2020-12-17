602338baa65fc980cbf1cf0ab1eb90b567e5d626
BilboHop <62094663+BilboHop@users.noreply.github.com>
2020-12-17 03:40:06 +0200

    Add files via upload
    Added package config with classes DataSourceConfig, HibernateConfig, JdbcConfig, JpaConfig;
    Added package controllers with classes HibernateController, IndexController, JdbcController, JpaController and interfaces CarBrandDAO, CarModelDAO;
    
    Added package dao with packages hibernate, jdbc, jpa;
    Package hibernate have classes CarBrandDAOHibernatelmpl, CarModelDAOHibernatelmpl;
    Package jdbc have classes CarBrandDAOJdbcImpl, CarModelDAOJdbcImpl;
    Package jpa have classes CarBrandDAOJpaImpl, CarModelDAOJpaImpl;
    
    Added package service with classes CarBrandHibernateSeviceImpl, CarBrandJdbcServiceImpl, CarBrandJpaServiceImpl, CarModelHibernateSeviceImpl, CarModelJdbcServiceImpl, CarModelJpaServiceImpl and interfaces CarBrandService, CarModelService ;
    
    Added package service with classes CarBrandHibernateSeviceImpl, CarBrandJdbcServiceImpl, CarBrandJpaServiceImpl, CarModelHibernateSeviceImpl, CarModelJdbcServiceImpl, CarModelJpaServiceImpl and interfaces CarBrandService, CarModelService ;
    
    Added package view with classes AbstractTextPdfView, AbstractPOIExcelView, ExcelBuilder, PDFBuilder;

---
+++ src/main/java/edu/javavt18/service/CarBrandJpaServiceImpl.java
@@ -0,0 +1,71 @@
+package edu.javavt18.service;
+
+import edu.javavt18.dao.CarBrandDAO;
+import edu.javavt18.model.CarBrand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * JPA implementation for {@link CarBrandService}.
+ *
+ * @see CarBrandService
+ */
+@Service("carBrandJpaService")
+@Transactional(readOnly = false, value = "jpaTransactionManager")
+public class CarBrandJpaServiceImpl implements CarBrandService {
+
+    /**
+     * An instance of {@link edu.javavt18.dao.jpa.CarBrandDAOJpaImpl}.
+     */
+    @Autowired
+    @Qualifier("getCarBrandJpaDAO")
+    private CarBrandDAO carBrandDAO;
+
+    /**
+     * <p>Persists {@link CarBrand} to the database either by adding it there or by updating.</p>
+     * <p>If {@code item} is not in the database yet, saves it there.
+     * Otherwise updates the row in the database with the same id as {@code item}.</p>
+     * <p>Uses JPA to interact with the database.</p>
+     *
+     * @param item item to persist to the database.
+     */
+    public void saveOrUpdate(CarBrand item) {
+        carBrandDAO.saveOrUpdate(item);
+    }
+
+    /**
+     * Deletes a {@link CarBrand} from the database with the given id.
+     * <p>Uses JPA to interact with the database.</p>
+     *
+     * @param itemId id of the item which should be deleted.
+     */
+    public void delete(int itemId) {
+        carBrandDAO.delete(itemId);
+    }
+
+    /**
+     * Selects single {@link CarBrand} from the database with the given id.
+     * <p>Uses JPA to interact with the database.</p>
+     *
+     * @param itemId id of the item which should be selected.
+     * @return {@link CarBrand} filled with data from the database row if a row with id = {@code itemId} exists there,
+     * otherwise {@code null}.
+     */
+    public CarBrand get(int itemId) {
+        return carBrandDAO.get(itemId);
+    }
+
+    /**
+     * Selects all {@link CarBrand}s from the database.
+     * <p>Uses JPA to interact with the database.</p>
+     *
+     * @return {@link List} of {@link CarBrand} filled with all data from the database (could be empty).
+     */
+    public List<CarBrand> list() {
+        return carBrandDAO.list();
+    }
+}
